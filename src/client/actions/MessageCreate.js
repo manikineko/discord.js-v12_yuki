701d01a84c712475aab1791546d9f1e16baac32a
Daisy <66263341+bloomsirenix@users.noreply.github.com>
2024-05-29 15:10:04 +0200

    fixed some stuff

--- src/client/actions/MessageCreate.js
+++ src/client/actions/MessageCreate.js
@@ -2,12 +2,25 @@
 
 const Action = require('./Action');
 const { Events } = require('../../util/Constants');
+const Message = require('../../structures/Message'); // Make sure this path is correct based on your project structure
 
 class MessageCreateAction extends Action {
   handle(data) {
     const client = this.client;
     const channel = client.channels.cache.get(data.channel_id);
     if (channel) {
+      if (!channel.messages) {
+        // Ensure channel.messages is defined
+        channel.messages = {
+          cache: new Map(),
+          add: function(data) {
+            const message = new Message(channel, data, client);
+            this.cache.set(data.id, message);
+            return message;
+          }
+        };
+      }
+      
       const existing = channel.messages.cache.get(data.id);
       if (existing) return { message: existing };
       const message = channel.messages.add(data);
